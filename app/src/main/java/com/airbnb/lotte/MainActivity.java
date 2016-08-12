853f5db290708f2337f6fb9cea691e0f052ec702
Gabriel Peal <gabriel.peal@airbnb.com>
2016-08-12 13:43:54 -0700

    Initial commit

---
+++ app/src/main/java/com/airbnb/lotte/MainActivity.java
@@ -0,0 +1,13 @@
+package com.airbnb.lotte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
