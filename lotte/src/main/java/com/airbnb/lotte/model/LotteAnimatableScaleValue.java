2d1cceaf751554c9b72c3019fb7ad2ae5721a710
Gabriel Peal <gabriel.peal@airbnb.com>
2016-09-11 23:28:04 -0700

    Implemented LotteAnimatableScaleValue but not tested.

--- lotte/src/main/java/com/airbnb/lotte/model/LotteAnimatableScaleValue.java
+++ lotte/src/main/java/com/airbnb/lotte/model/LotteAnimatableScaleValue.java
@@ -1,8 +1,11 @@
 package com.airbnb.lotte.model;
 
-import android.graphics.Point;
+import android.graphics.PointF;
+import android.support.v4.view.animation.PathInterpolatorCompat;
 import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 
+import com.airbnb.lotte.utils.JsonUtils;
 import com.airbnb.lotte.utils.LotteTransform3D;
 
 import org.json.JSONArray;
@@ -17,9 +20,9 @@ public class LotteAnimatableScaleValue implements LotteAnimatableValue {
     private static final String TAG = LotteAnimatableScaleValue.class.getSimpleName();
 
     private LotteTransform3D initialScale;
-    private final List<Point> scaleKeyframes = new ArrayList<>();
+    private final List<LotteTransform3D> scaleKeyframes = new ArrayList<>();
     private final List<Float> keyTimes = new ArrayList<>();
-    private final List<Interpolator> timingFunctions = new ArrayList<>();
+    private final List<Interpolator> interpolators = new ArrayList<>();
 
     private float delay;
     private float duration;
@@ -50,7 +53,82 @@ public class LotteAnimatableScaleValue implements LotteAnimatableValue {
     }
 
     private void buildAnimationForKeyframes(JSONArray keyframes) {
-        // TODO
+        try {
+            startFrame = keyframes.getJSONObject(0).getInt("t");
+            int endFrame = keyframes.getJSONObject(keyframes.length() - 1).getInt("t");
+
+            if (endFrame <= startFrame) {
+                throw new IllegalArgumentException("End frame must be after start frame " + endFrame + " vs " + startFrame);
+            }
+
+            durationFrames = endFrame - startFrame;
+
+            duration = durationFrames / frameRate;
+            delay = startFrame / frameRate;
+
+            boolean addStartValue = true;
+            boolean addTimePadding = false;
+            LotteTransform3D outValue = null;
+
+            for (int i = 0; i < keyframes.length(); i++) {
+                JSONObject keyframe = keyframes.getJSONObject(i);
+                int frame = keyframe.getInt("t");
+                float timePercentage = (frame - startFrame) / (float) durationFrames;
+
+                if (outValue != null) {
+                    scaleKeyframes.add(outValue);
+                    interpolators.add(new LinearInterpolator());
+                    outValue = null;
+                }
+
+                LotteTransform3D startValue = null;
+                if (addStartValue) {
+                    if (keyframe.has("s")) {
+                        startValue = xformForValueArray(keyframe.getJSONArray("s"));
+                        if (i == 0) {
+                            initialScale = startValue;
+                        }
+                        scaleKeyframes.add(startValue);
+                        if (!interpolators.isEmpty()) {
+                            interpolators.add(new LinearInterpolator());
+                        }
+                    }
+                    addStartValue = false;
+                }
+
+                if (addTimePadding) {
+                    float holdPercentage = timePercentage - 0.00001f;
+                    keyTimes.add(holdPercentage);
+                    addTimePadding = false;
+                }
+
+                if (keyframe.has("e")) {
+                    LotteTransform3D endValue = xformForValueArray(keyframe.getJSONArray("e"));
+                    scaleKeyframes.add(endValue);
+
+                    Interpolator interpolator;
+
+                    if (keyframe.has("o") && keyframe.has("i")) {
+                        PointF cp1 = JsonUtils.pointValueFromDict(keyframe.getJSONObject("o"));
+                        PointF cp2 = JsonUtils.pointValueFromDict(keyframe.getJSONObject("i"));
+                        interpolator = PathInterpolatorCompat.create(cp1.x, cp1.y, cp2.x, cp2.y);
+                    } else {
+                        interpolator = new LinearInterpolator();
+                    }
+                    interpolators.add(interpolator);
+                }
+
+                keyTimes.add(timePercentage);
+
+                if (keyframe.has("h") && keyframe.getBoolean("h")) {
+                    outValue = startValue;
+                    addStartValue = true;
+                    addTimePadding = true;
+                }
+            }
+        } catch (JSONException e) {
+            throw new IllegalArgumentException("Unable to parse scale animation " + keyframes, e);
+        }
     }
 
     private LotteTransform3D xformForValueArray(JSONArray value) {
