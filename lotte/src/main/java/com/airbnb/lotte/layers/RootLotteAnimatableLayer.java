0dd6a55210fe1f8ab9daa3db524cac9becaa382e
Gabriel Peal <gabriel.peal@airbnb.com>
2016-09-24 13:28:19 -0700

    Remove transform from root layer

--- lotte/src/main/java/com/airbnb/lotte/layers/RootLotteAnimatableLayer.java
+++ lotte/src/main/java/com/airbnb/lotte/layers/RootLotteAnimatableLayer.java
@@ -4,9 +4,6 @@ import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 
-import com.airbnb.lotte.utils.LotteTransform3D;
-import com.airbnb.lotte.utils.Observable;
-
 public class RootLotteAnimatableLayer extends LotteAnimatableLayer {
     public RootLotteAnimatableLayer(Drawable.Callback callback) {
         super(0, callback);
@@ -14,7 +11,6 @@ public class RootLotteAnimatableLayer extends LotteAnimatableLayer {
 
     @Override
     public void draw(@NonNull Canvas canvas) {
-        setTransform(new Observable<>(new LotteTransform3D()));
         super.draw(canvas);
         canvas.clipRect(getBounds());
     }
