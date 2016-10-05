0b3506bed35fae5b90015a00cd18e5d006972ef1
Gabriel Peal <gabriel.peal@airbnb.com>
2016-10-04 22:27:39 -0400

    Removed trim path caching because it causes some animations (like alarm) to look bad

--- lotte/src/main/java/com/airbnb/lotte/layers/LotteShapeLayer.java
+++ lotte/src/main/java/com/airbnb/lotte/layers/LotteShapeLayer.java
@@ -13,7 +13,6 @@ import android.graphics.drawable.Drawable;
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.airbnb.lotte.animation.LotteAnimatableNumberValue;
 import com.airbnb.lotte.model.LotteShapeStroke;
@@ -26,8 +25,6 @@ import java.util.List;
  * Mimics CAShapeLayer
  */
 public class LotteShapeLayer extends LotteAnimatableLayer {
-    private static final float MIN_TRIM_PATH_CHANGE_DELTA = 7;
-
     private final Observable.OnChangedListener pathChangedListener = new Observable.OnChangedListener() {
         @Override
         public void onChanged() {
@@ -86,8 +83,6 @@ public class LotteShapeLayer extends LotteAnimatableLayer {
     private Observable<Number> lineWidth;
     @Nullable private Observable<Number> strokeStart;
     @Nullable private Observable<Number> strokeEnd;
-    private float lastMeasuredStrokeStart = Float.MIN_VALUE;
-    private float lastMeasuredStrokeEnd = Float.MIN_VALUE;
 
     private Observable<Number> shapeAlpha;
     private Observable<Number> transformAlpha;
@@ -306,13 +301,6 @@ public class LotteShapeLayer extends LotteAnimatableLayer {
         if (strokeStart != null && strokeEnd != null) {
             float strokeStartVal = (float) strokeStart.getValue();
             float strokeEndVal = (float) strokeEnd.getValue();
-            if (Math.abs(strokeStartVal - lastMeasuredStrokeStart) < MIN_TRIM_PATH_CHANGE_DELTA &&
-                    Math.abs(strokeEndVal - lastMeasuredStrokeEnd) < MIN_TRIM_PATH_CHANGE_DELTA) {
-                return;
-            }
-            lastMeasuredStrokeStart = strokeStartVal;
-            lastMeasuredStrokeEnd = strokeEndVal;
-
             float length = pathMeasure.getLength();
             float start = length * strokeStartVal / 100f;
             float end = length * strokeEndVal / 100f;
