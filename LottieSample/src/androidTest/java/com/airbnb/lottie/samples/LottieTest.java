b0b7ddac80b9770dea2ee53783cd812c4fd67ccd
Gabriel Peal <gabriel.peal@airbnb.com>
2016-10-15 16:19:05 -0700

    Updated JSON

--- LottieSample/src/androidTest/java/com/airbnb/lottie/samples/LottieTest.java
+++ LottieSample/src/androidTest/java/com/airbnb/lottie/samples/LottieTest.java
@@ -5,6 +5,9 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import org.junit.Test;
 
+/**
+ * Run these with: ./gradlew --daemon recordMode screenshotTests
+ */
 public class LottieTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     public LottieTest() {
@@ -13,15 +16,21 @@ public class LottieTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     @Test
     public void testAll() {
-        TestRobot.testStaticFile(getActivity(), "Hosts Still.json");
-        TestRobot.testAnimatedFile(getActivity(), "BusinessTravel01.json");
-        TestRobot.testAnimatedFile(getActivity(), "BusinessTravel03.json");
-        TestRobot.testAnimatedFile(getActivity(), "LightBulb.json");
-        TestRobot.testAnimatedFile(getActivity(), "Diamond 2.json");
-        TestRobot.testAnimatedFile(getActivity(), "CheckSwitch.json");
         TestRobot.testAnimatedFile(getActivity(), "Alarm.json");
+        TestRobot.testAnimatedFile(getActivity(), "BusinessTravel01.json");
+        TestRobot.testAnimatedFile(getActivity(), "BusinessTravel02.json");
+        TestRobot.testAnimatedFile(getActivity(), "BusinessTravel03.json");
+        TestRobot.testAnimatedFile(getActivity(), "CheckSwitch.json");
+        TestRobot.testAnimatedFile(getActivity(), "Diamond.json");
+        TestRobot.testAnimatedFile(getActivity(), "Heart.json");
+        TestRobot.testAnimatedFile(getActivity(), "Hosts.json");
+        TestRobot.testAnimatedFile(getActivity(), "House.json");
+        TestRobot.testAnimatedFile(getActivity(), "Identity_AllSet.json");
+        TestRobot.testAnimatedFile(getActivity(), "Identity_City.json");
+        TestRobot.testAnimatedFile(getActivity(), "Identity_GovtID.json");
+        TestRobot.testAnimatedFile(getActivity(), "Identity_Selfie.json");
+        TestRobot.testAnimatedFile(getActivity(), "Lightbulb.json");
         TestRobot.testAnimatedFile(getActivity(), "Stars.json");
         TestRobot.testAnimatedFile(getActivity(), "LoopPlayOnce.json");
-        TestRobot.testAnimatedFile(getActivity(), "City.json");
     }
 }
--- LottieSample/src/androidTest/java/com/airbnb/lottie/samples/TestRobot.java
+++ LottieSample/src/androidTest/java/com/airbnb/lottie/samples/TestRobot.java
@@ -1,7 +1,5 @@
 package com.airbnb.lottie.samples;
 
-import android.content.Context;
-
 import com.airbnb.lottie.LottieAnimationView;
 import com.facebook.testing.screenshot.Screenshot;
 import com.facebook.testing.screenshot.ViewHelpers;
@@ -12,16 +10,16 @@ class TestRobot {
     private static final float[] DEFAULT_ANIMATED_PROGRESS = {0f, 0.05f, 0.10f, 0.15f, 0.20f, 0.25f, 0.30f, 0.40f, 0.50f, 0.60f, 0.70f, 0.80f, 0.90f, 1f};
     private static final float[] DEFAULT_STATIC_PROGRESS = {0f};
 
-    static void testAnimatedFile(Context context, String fileName) {
-        testFile(context, fileName, DEFAULT_ANIMATED_PROGRESS);
+    static void testAnimatedFile(MainActivity activity, String fileName) {
+        testFile(activity, fileName, DEFAULT_ANIMATED_PROGRESS);
     }
 
-    static void testStaticFile(Context context, String fileName) {
-        testFile(context, fileName, DEFAULT_STATIC_PROGRESS);
+    static void testStaticFile(MainActivity activity, String fileName) {
+        testFile(activity, fileName, DEFAULT_STATIC_PROGRESS);
     }
 
-    private static void testFile(Context context, String fileName, float[] progress) {
-        LottieAnimationView view = new LottieAnimationView(context);
+    private static void testFile(MainActivity activity, String fileName, float[] progress) {
+        LottieAnimationView view = new LottieAnimationView(activity);
         view.setAnimationSync(fileName);
         ViewHelpers.setupView(view)
                 .layout();
