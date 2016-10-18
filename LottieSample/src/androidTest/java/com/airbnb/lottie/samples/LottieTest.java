ac19c4be8f1cebc0f0018450a9ddc45650968099
Gabriel Peal <gabriel.peal@airbnb.com>
2016-10-18 13:50:02 -0700

    Updated screenshots

--- LottieSample/src/androidTest/java/com/airbnb/lottie/samples/LottieTest.java
+++ LottieSample/src/androidTest/java/com/airbnb/lottie/samples/LottieTest.java
@@ -18,17 +18,11 @@ public class LottieTest extends ActivityInstrumentationTestCase2<MainActivity> {
     public void testAll() {
         TestRobot.testAnimatedFile(getActivity(), "Alarm.json");
         TestRobot.testAnimatedFile(getActivity(), "BusinessTravel01.json");
-        TestRobot.testAnimatedFile(getActivity(), "BusinessTravel02.json");
         TestRobot.testAnimatedFile(getActivity(), "BusinessTravel03.json");
         TestRobot.testAnimatedFile(getActivity(), "CheckSwitch.json");
         TestRobot.testAnimatedFile(getActivity(), "Diamond.json");
         TestRobot.testAnimatedFile(getActivity(), "Heart.json");
         TestRobot.testAnimatedFile(getActivity(), "Hosts.json");
-        TestRobot.testAnimatedFile(getActivity(), "House.json");
-        TestRobot.testAnimatedFile(getActivity(), "Identity_AllSet.json");
-        TestRobot.testAnimatedFile(getActivity(), "Identity_City.json");
-        TestRobot.testAnimatedFile(getActivity(), "Identity_GovtID.json");
-        TestRobot.testAnimatedFile(getActivity(), "Identity_Selfie.json");
         TestRobot.testAnimatedFile(getActivity(), "LightBulb.json");
         TestRobot.testAnimatedFile(getActivity(), "Stars.json");
         TestRobot.testAnimatedFile(getActivity(), "LoopPlayOnce.json");
