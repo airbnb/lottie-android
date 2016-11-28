5bbf61b082b73ea9257aa4be13a852b23bea6865
Gabriel Peal <gabriel.peal@airbnb.com>
2016-11-28 09:18:00 -0500

    Added a caching strategy to LottieAnimationView#setAnimation

--- lottie/src/main/java/com/airbnb/lottie/LottieAnimationView.java
+++ lottie/src/main/java/com/airbnb/lottie/LottieAnimationView.java
@@ -25,8 +25,11 @@ import com.airbnb.lottie.layers.RootAnimatableLayer;
 import com.airbnb.lottie.model.Layer;
 import com.airbnb.lottie.model.LottieComposition;
 
+import java.lang.ref.WeakReference;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This view will load, deserialize, and display an After Effects animation exported with
@@ -40,6 +43,16 @@ import java.util.List;
  */
 public class LottieAnimationView extends ImageView {
 
+    /**
+     * Caching strategy for compositions that will be reused frequently.
+     * Weak or Strong indicates the GC reference strength of the composition in the cache.
+     */
+    public enum CacheStrategy {
+        None,
+        Weak,
+        Strong
+    }
+
 
     /**
      * Returns a {@link LottieAnimationView} that will allow it to be used without being attached to a window.
@@ -52,12 +65,8 @@ public class LottieAnimationView extends ImageView {
         return view;
     }
 
-    private final LottieComposition.OnCompositionLoadedListener loadedListener = new LottieComposition.OnCompositionLoadedListener() {
-        @Override
-        public void onCompositionLoaded(LottieComposition composition) {
-            setComposition(composition);
-        }
-    };
+    @Nullable private static Map<String, LottieComposition> strongRefCache;
+    @Nullable private static Map<String, WeakReference<LottieComposition>> weakRefCache;
 
     private final LongSparseArray<LayerView> layerMap = new LongSparseArray<>();
     private final RootAnimatableLayer rootAnimatableLayer = new RootAnimatableLayer(this);
@@ -189,17 +198,50 @@ public class LottieAnimationView extends ImageView {
         }
     }
 
+    public void setAnimation(String animationName) {
+        setAnimation(animationName, CacheStrategy.None);
+    }
+
     /**
      * Sets the animation from a file in the assets directory.
      * This will load and deserialize the file asynchronously.
      */
-    public void setAnimation(final String animationName) {
+    @SuppressWarnings("WeakerAccess")
+    public void setAnimation(final String animationName, final CacheStrategy cacheStrategy) {
+        this.animationName = animationName;
+        if (weakRefCache != null && weakRefCache.containsKey(animationName)) {
+            WeakReference<LottieComposition> compRef = weakRefCache.get(animationName);
+            if (compRef.get() != null) {
+                setComposition(compRef.get());
+                return;
+            }
+        } else if (strongRefCache != null && strongRefCache.containsKey(animationName)) {
+            setComposition(strongRefCache.get(animationName));
+            return;
+        }
+
         isAnimationLoading = true;
         setProgressWhenCompositionSet = false;
         playAnimationWhenCompositionSet = false;
 
-        this.animationName = animationName;
-        LottieComposition.fromFile(getContext(), animationName, loadedListener);
+        LottieComposition.fromFile(getContext(), animationName, new LottieComposition.OnCompositionLoadedListener() {
+            @Override
+            public void onCompositionLoaded(LottieComposition composition) {
+                if (cacheStrategy == CacheStrategy.Strong) {
+                    if (strongRefCache == null) {
+                        strongRefCache = new HashMap<>();
+                    }
+                    strongRefCache.put(animationName, composition);
+                } else if (cacheStrategy == CacheStrategy.Weak) {
+                    if (weakRefCache == null) {
+                        weakRefCache = new HashMap<>();
+                    }
+                    weakRefCache.put(animationName, new WeakReference<>(composition));
+                }
+
+                setComposition(composition);
+            }
+        });
     }
 
     public void setComposition(@NonNull LottieComposition composition) {
