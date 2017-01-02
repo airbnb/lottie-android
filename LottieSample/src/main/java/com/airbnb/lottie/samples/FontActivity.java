d5d66c2b7724d512f51bc258a80cbbc7e44bc8ae
Gabriel Peal <gabriel.peal@airbnb.com>
2017-01-01 21:03:37 -0800

    Moved font handling from the activity to the view group

--- LottieSample/src/main/java/com/airbnb/lottie/samples/FontActivity.java
+++ LottieSample/src/main/java/com/airbnb/lottie/samples/FontActivity.java
@@ -3,21 +3,11 @@ package com.airbnb.lottie.samples;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
-import android.view.ViewGroup;
-
-import com.airbnb.lottie.LottieAnimationView;
-import com.airbnb.lottie.model.LottieComposition;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class FontActivity extends AppCompatActivity {
-
-    private Map<Character, LottieComposition> compositionMap = new HashMap<>();
 
     @BindView(R.id.font_view) LottieFontViewGroup fontView;
 
@@ -26,50 +16,5 @@ public class FontActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_font);
         ButterKnife.bind(this);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_SPACE) {
-            fontView.addSpace();
-            return true;
-        }
-
-        if (keyCode == KeyEvent.KEYCODE_DEL) {
-            fontView.removeLastView();
-            return true;
-        }
-
-        if (event.getKeyCode() < KeyEvent.KEYCODE_A || event.getKeyCode() > KeyEvent.KEYCODE_Z) {
-            return super.onKeyDown(keyCode, event);
-        }
-
-
-        final char letter = Character.toUpperCase((char) event.getUnicodeChar());
-        if (compositionMap.containsKey(letter)) {
-            addComposition(compositionMap.get(letter));
-        } else {
-            String fileName = "Amelie/" + letter + ".json";
-            LottieComposition.fromAssetFileName(this, fileName, new LottieComposition.OnCompositionLoadedListener() {
-                @Override
-                public void onCompositionLoaded(LottieComposition composition) {
-                    compositionMap.put(letter, composition);
-                    addComposition(composition);
-                }
-            });
-        }
-
-        return true;
-    }
-
-    private void addComposition(LottieComposition composition) {
-        LottieAnimationView lottieAnimationView = new LottieAnimationView(this);
-        lottieAnimationView.setLayoutParams(new LottieFontViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        ));
-        lottieAnimationView.setComposition(composition);
-        lottieAnimationView.playAnimation();
-        fontView.addView(lottieAnimationView);
     }
 }
--- LottieSample/src/main/java/com/airbnb/lottie/samples/LottieFontViewGroup.java
+++ LottieSample/src/main/java/com/airbnb/lottie/samples/LottieFontViewGroup.java
@@ -1,28 +1,46 @@
 package com.airbnb.lottie.samples;
 
 import android.content.Context;
+import android.text.InputType;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.BaseInputConnection;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 import android.widget.FrameLayout;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.model.LottieComposition;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottieFontViewGroup extends FrameLayout {
 
+    private final Map<Character, LottieComposition> compositionMap = new HashMap<>();
     private final List<View> views = new ArrayList<>();
 
     public LottieFontViewGroup(Context context) {
         super(context);
+        init();
     }
 
     public LottieFontViewGroup(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public LottieFontViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        setFocusableInTouchMode(true);
     }
 
     void addSpace() {
@@ -63,6 +81,65 @@ public class LottieFontViewGroup extends FrameLayout {
             view.layout(currentX, currentY, currentX + view.getMeasuredWidth(), currentY + view.getMeasuredHeight());
             currentX += view.getWidth();
         }
+    }
+
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+        BaseInputConnection fic = new BaseInputConnection(this, false);
+        outAttrs.actionLabel = null;
+        outAttrs.inputType = InputType.TYPE_NULL;
+        outAttrs.imeOptions = EditorInfo.IME_ACTION_NEXT;
+        return fic;
+    }
+
+    @Override
+    public boolean onCheckIsTextEditor() {
+        return true;
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_SPACE) {
+            addSpace();
+            return true;
+        }
+
+        if (keyCode == KeyEvent.KEYCODE_DEL) {
+            removeLastView();
+            return true;
+        }
+
+        if (event.getKeyCode() < KeyEvent.KEYCODE_A || event.getKeyCode() > KeyEvent.KEYCODE_Z) {
+            return super.onKeyDown(keyCode, event);
+        }
+
+
+        final char letter = Character.toUpperCase((char) event.getUnicodeChar());
+        if (compositionMap.containsKey(letter)) {
+            addComposition(compositionMap.get(letter));
+        } else {
+            String fileName = "Amelie/" + letter + ".json";
+            LottieComposition.fromAssetFileName(getContext(), fileName, new LottieComposition.OnCompositionLoadedListener() {
+                @Override
+                public void onCompositionLoaded(LottieComposition composition) {
+                    compositionMap.put(letter, composition);
+                    addComposition(composition);
+                }
+            });
+        }
+
+        return true;
+    }
+
+    private void addComposition(LottieComposition composition) {
+        LottieAnimationView lottieAnimationView = new LottieAnimationView(getContext());
+        lottieAnimationView.setLayoutParams(new LottieFontViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        lottieAnimationView.setComposition(composition);
+        lottieAnimationView.playAnimation();
+        addView(lottieAnimationView);
     }
 
     private boolean fitsOnCurrentLine(int currentX, View view) {
