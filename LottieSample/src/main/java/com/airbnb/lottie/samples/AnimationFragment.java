f419444e3e775afd8cb239d83e65408b8c684a81
Gabriel Peal <gabriel.peal@airbnb.com>
2017-01-24 17:56:15 -0800

    Allow loop to be turned back on

--- LottieSample/src/main/java/com/airbnb/lottie/samples/AnimationFragment.java
+++ LottieSample/src/main/java/com/airbnb/lottie/samples/AnimationFragment.java
@@ -188,7 +188,7 @@ public class AnimationFragment extends Fragment {
 
     @OnClick(R.id.loop)
     void onLoopChanged() {
-        loopButton.setEnabled(!loopButton.isEnabled());
+        loopButton.setActivated(!loopButton.isActivated());
         animationView.loop(loopButton.isEnabled());
     }
 
