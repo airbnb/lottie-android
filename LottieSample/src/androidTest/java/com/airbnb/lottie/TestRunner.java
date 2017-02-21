ec8d6f87cb266d3edf933fb9b236bbba699430db
Felipe Lima <felipe.lima@gmail.com>
2017-02-20 18:16:57 -0800

    Update tests to JUnit 4

--- LottieSample/src/androidTest/java/com/airbnb/lottie/TestRunner.java
+++ LottieSample/src/androidTest/java/com/airbnb/lottie/TestRunner.java
@@ -1,7 +1,18 @@
 package com.airbnb.lottie;
 
+import android.os.Bundle;
 import android.support.test.runner.AndroidJUnitRunner;
 
+import com.facebook.testing.screenshot.ScreenshotRunner;
 
 public class TestRunner extends AndroidJUnitRunner {
+  @Override public void onCreate(Bundle arguments) {
+    ScreenshotRunner.onCreate(this, arguments);
+    super.onCreate(arguments);
+  }
+
+  @Override public void finish(int resultCode, Bundle results) {
+    ScreenshotRunner.onDestroy();
+    super.finish(resultCode, results);
+  }
 }
