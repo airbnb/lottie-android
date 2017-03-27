dbf70939aee7473cbcff3bd9474e34ac3838272f
Sean F Chan <seaniec88@gmail.com>
2017-03-27 14:28:13 -0700

    add color filter documentation to README (#219)

---
+++ lottie/src/main/java/com/airbnb/lottie/SimpleColorFilter.java
@@ -0,0 +1,19 @@
+package com.airbnb.lottie;
+
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.support.annotation.ColorInt;
+
+/**
+ * A color filter with a predefined transfer mode that applies the specified color on top of the
+ * original color. As there are many other transfer modes, please take a look at the definition
+ * of PorterDuff.Mode.SRC_ATOP to find one that suits your needs.
+ * This site has a great explanation of Porter/Duff compositing algebra as well as a visual
+ * representation of many of the transfer modes:
+ * http://ssp.impulsetrain.com/porterduff.html
+ */
+public class SimpleColorFilter extends PorterDuffColorFilter {
+  public SimpleColorFilter(@ColorInt int color) {
+    super(color, PorterDuff.Mode.SRC_ATOP);
+  }
+}
