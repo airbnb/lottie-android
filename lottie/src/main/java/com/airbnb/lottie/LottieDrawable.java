22af83c97ea3c5f054664f3cc534bee9982a411c
Gabriel Peal <gabriel.peal@airbnb.com>
2017-04-04 22:07:21 -0700

    Expose setScale on LottieAnimationView and LottieDrawable
    Also show people the ideal scale as well as a slider to change it
    in AnimationFragment.
    
    Fixes #240

--- lottie/src/main/java/com/airbnb/lottie/LottieDrawable.java
+++ lottie/src/main/java/com/airbnb/lottie/LottieDrawable.java
@@ -363,6 +363,15 @@ public class LottieDrawable extends Drawable implements Drawable.Callback {
     return progress;
   }
 
+  /**
+   * Set the scale on the current composition. The only cost of this function is re-rendering the
+   * current frame so you may call it frequent to scale something up or down.
+   *
+   * The smaller the animation is, the better the performance will be. You may find that scaling an
+   * animation down then rendering it in a larger ImageView and letting ImageView scale it back up
+   * with a scaleType such as centerInside will yield better performance with little perceivable
+   * quality loss.
+   */
   @SuppressWarnings("WeakerAccess") public void setScale(float scale) {
     this.scale = scale;
     updateBounds();
