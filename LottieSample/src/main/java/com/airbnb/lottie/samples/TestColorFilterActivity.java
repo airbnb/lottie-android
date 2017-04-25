452de51c5eabedbf70ef0a01affb44b9b72e08a7
Ramona Harrison <ramona.harrison@nytimes.com>
2017-04-25 15:22:26 -0400

    Add tests for XML color filter

---
+++ LottieSample/src/main/java/com/airbnb/lottie/samples/TestColorFilterActivity.java
@@ -0,0 +1,13 @@
+package com.airbnb.lottie.samples;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class TestColorFilterActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_test_color_filter);
+  }
+}
