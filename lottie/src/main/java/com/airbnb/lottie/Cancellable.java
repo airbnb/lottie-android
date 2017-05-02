airbnb/lottie-android

commit 7d25209609e8ca67104704f5e16a50b7aafc4938

Cancellable is returned by public functions, but isn't accessible (#157)
* Cancellable is returned by public functions, but isn't accessible

* add cancel into exampl on README and fix fromJson example to use Factory

lottie/src/main/java/com/airbnb/lottie/Cancellable.java
@@ -1,5 +1,5 @@
 package com.airbnb.lottie;
 
-interface Cancellable {
+public interface Cancellable {
   void cancel();
 }

commit b6bb36e36305058a2b337f3f07622b290f3bcb61

Make Cancellable interface visible
This is a regression since https://github.com/airbnb/lottie-android/pull/157
Cancellable is returned  from the composition factory but is inaccessible.

lottie/src/main/java/com/airbnb/lottie/Cancellable.java
@@ -1,5 +1,5 @@
 package com.airbnb.lottie;
 
-interface Cancellable {
+public interface Cancellable {
   void cancel();
 }