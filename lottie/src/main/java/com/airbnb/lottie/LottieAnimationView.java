3867cdd91a18186fd12a3f96ec46a1d7849b0548
Cyril Mottier <cyril@cyrilmottier.com>
2017-10-13 11:23:59 +0200

    Various cleans (#498)
    * Remove a useless this prefix
    
    * Do not set the animationName twice
    
    The assignation is already done at the beginning of the method and the argument animationName is final and hence can’t change.
    
    * Fix tiny code style issues

--- lottie/src/main/java/com/airbnb/lottie/LottieAnimationView.java
+++ lottie/src/main/java/com/airbnb/lottie/LottieAnimationView.java
@@ -216,7 +216,7 @@ import java.util.Map;
 
     SavedState ss = (SavedState) state;
     super.onRestoreInstanceState(ss.getSuperState());
-    this.animationName = ss.animationName;
+    animationName = ss.animationName;
     if (!TextUtils.isEmpty(animationName)) {
       setAnimation(animationName);
     }
@@ -248,7 +248,7 @@ import java.util.Map;
     // AppCompatImageView constructor will set the image when set from xml
     // before LottieDrawable has been initialized
     if (lottieDrawable != null) {
-        lottieDrawable.recycleBitmaps();
+      lottieDrawable.recycleBitmaps();
     }
   }
 
@@ -336,7 +336,6 @@ import java.util.Map;
       return;
     }
 
-    this.animationName = animationName;
     lottieDrawable.cancelAnimation();
     cancelLoaderTask();
     compositionLoader = LottieComposition.Factory.fromAssetFileName(getContext(), animationName,
