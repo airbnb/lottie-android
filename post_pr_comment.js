4afcf18601250032db38149b2f6964aefef362d8
Gabriel Peal <gabriel.peal@airbnb.com>
2017-12-14 15:11:06 -0500

    Firebase

--- post_pr_comment.js
+++ post_pr_comment.js
@@ -6,13 +6,7 @@ if (!process.env.TRAVIS_REPO_SLUG) {
 
 const https = require('https');
 
-let postData;
-console.log('Firebase exit code: ' + process.env.FIREBASE_EXIT_CODE);
-if (process.env.FIREBASE_EXIT_CODE) {
-  postData = `{\"body\": \"Firebase tests failed.\"}`
-} else {
-  postData = `{\"body\": \"Snapshot Test Results: https://happo.io/compare?q=lottie-master-android26..lottie-${process.env.GIT_SHA}-android26&a=4\"}`
-}
+const postData = `{\"body\": \"Snapshot Test Results: https://happo.io/compare?q=lottie-master-android26..lottie-${process.env.GIT_SHA}-android26&a=4\"}`
 
 const options = {
   hostname: 'api.github.com',
