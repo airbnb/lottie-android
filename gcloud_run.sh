9cb7d7555540a21f17641a589ac428b2b73be0f7
Gabriel Peal <gabriel.peal@airbnb.com>
2018-02-17 17:23:30 -0800

    Updated travis

---
+++ gcloud_run.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+if [ "$TRAVIS_PULL_REQUEST_SLUG" != "airbnb/lottie-android" ]
+  echo "Skipping gcloud run for PR because api keys are not available from forks."
+  exit 0
+fi
+
+gcloud firebase test android run --type instrumentation --device model=Nexus5X,version=26 --app LottieSample/build/outputs/apk/debug/LottieSample-debug.apk --test LottieSample/build/outputs/apk/androidTest/debug/LottieSample-debug-androidTest.apk
+./post_pr_comment.js
