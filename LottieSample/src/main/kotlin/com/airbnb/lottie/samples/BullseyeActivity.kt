488620eb0ee7d9df233473fbdb072e32340d3683
Gabriel Peal <gabriel.peal@airbnb.com>
2018-03-10 14:56:24 -0800

    [Sample App] Deleted the old animation and list fragment

--- LottieSample/src/main/kotlin/com/airbnb/lottie/samples/BullseyeActivity.kt
+++ LottieSample/src/main/kotlin/com/airbnb/lottie/samples/BullseyeActivity.kt
@@ -13,8 +13,6 @@ import kotlinx.android.synthetic.main.activity_bullseye.*
 
 class BullseyeActivity : AppCompatActivity() {
 
-    private var firstKeypath: KeyPath? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bullseye)
