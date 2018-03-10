0babb2c0abe0edaedf6cf960cbde74014b61e667
Gabriel Peal <gabriel.peal@airbnb.com>
2018-03-10 15:03:04 -0800

    Update travis config

--- gcloud_run.sh
+++ gcloud_run.sh
@@ -4,5 +4,14 @@ if [ "$TRAVIS_PULL_REQUEST_SLUG" != "airbnb/lottie-android" ]; then
   exit 0
 fi
 
+
+echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
+curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-182.0.0-linux-x86_64.tar.gz -o gcloud.tar.gz
+tar xzf gcloud.tar.gz -C ${HOME}
+${HOME}/google-cloud-sdk/install.sh --quiet --usage-reporting false
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+  # - gcloud components update
+gcloud config set project lottie-snapshots
+
 gcloud firebase test android run --type instrumentation --device model=Nexus5X,version=26 --app LottieSample/build/outputs/apk/debug/LottieSample-debug.apk --test LottieSample/build/outputs/apk/androidTest/debug/LottieSample-debug-androidTest.apk
 ./post_pr_comment.js
