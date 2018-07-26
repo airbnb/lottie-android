9bc07763d45d5c34b412118607291fc3c7b36ebb
Gabriel Peal <gabriel.peal@airbnb.com>
2018-07-25 21:44:51 -0700

    Fixed path

--- gcloud_run.sh
+++ gcloud_run.sh
@@ -12,7 +12,7 @@ if [ ! -f ${HOME}/google-cloud-sdk/install.sh ]; then
   tar xzf gcloud.tar.gz -C ${HOME}
   ${HOME}/google-cloud-sdk/install.sh --quiet --usage-reporting false
 fi
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/google-cloud-sdk/gcloud-service-key.json
 gcloud config set project lottie-snapshots
 
 gcloud firebase test android run --no-auto-google-login --type instrumentation --device model=Nexus5X,version=26 --app LottieSample/build/outputs/apk/debug/LottieSample-debug.apk --test LottieSample/build/outputs/apk/androidTest/debug/LottieSample-debug-androidTest.apk
