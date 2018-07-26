f7f6da2d00e7f713c1db7f6df7b5a8d0633ce12a
Gabriel Peal <gabriel.peal@airbnb.com>
2018-07-25 21:28:02 -0700

    WIP

--- gcloud_run.sh
+++ gcloud_run.sh
@@ -4,13 +4,15 @@ if [ -z $TRAVIS_PULL_REQUEST_SLUG ] && [ "$TRAVIS_REPO_SLUG" != "airbnb/lottie-a
   exit 0
 fi
 
-
-echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
-curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-182.0.0-linux-x86_64.tar.gz -o gcloud.tar.gz
-tar xzf gcloud.tar.gz -C ${HOME}
-${HOME}/google-cloud-sdk/install.sh --quiet --usage-reporting false
+if [ ! -f ${HOME}/google-cloud-sdk/install.sh ]; then
+  mkdir $HOME/.cache
+  echo "File not found!"
+  echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/.cache/gcloud-service-key.json
+  curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-209.0.0-linux-x86_64.tar.gz -o gcloud.tar.gz
+  tar xzf gcloud.tar.gz -C ${HOME}
+  ${HOME}/google-cloud-sdk/install.sh --quiet --usage-reporting false
+fi
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-  # - gcloud components update
 gcloud config set project lottie-snapshots
 
 gcloud firebase test android run --no-auto-google-login --type instrumentation --device model=Nexus5X,version=26 --app LottieSample/build/outputs/apk/debug/LottieSample-debug.apk --test LottieSample/build/outputs/apk/androidTest/debug/LottieSample-debug-androidTest.apk
