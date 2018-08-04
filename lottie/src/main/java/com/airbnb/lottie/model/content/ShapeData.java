c1708b66b7b85b97f4b04102a2f53cdf6f795de9
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-04 10:57:05 -0700

    Moved points inside check

--- lottie/src/main/java/com/airbnb/lottie/model/content/ShapeData.java
+++ lottie/src/main/java/com/airbnb/lottie/model/content/ShapeData.java
@@ -56,10 +56,9 @@ public class ShapeData {
       L.warn("Curves must have the same number of control points. Shape 1: " +
           shapeData1.getCurves().size() + "\tShape 2: " + shapeData2.getCurves().size());
     }
-
-    int points = Math.min(shapeData1.getCurves().size(), shapeData2.getCurves().size());
-
+    
     if (curves.isEmpty()) {
+      int points = Math.min(shapeData1.getCurves().size(), shapeData2.getCurves().size());
       for (int i = 0; i < points; i++) {
         curves.add(new CubicCurveData());
       }
