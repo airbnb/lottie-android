293f296d8fc44af22ddcdbeccc0318fb3fa1b3ba
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-04 14:09:13 -0700

    Cleaned up script

--- sign_and_upload.sh
+++ sign_and_upload.sh
@@ -11,19 +11,12 @@ echo "----------Building Release"
 echo "----------Signing APK"
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore lottie-upload-key.jks -storepass $LOTTIE_UPLOAD_CERT_KEY_STORE_PASSWORD LottieSample/build/outputs/apk/release/LottieSample-release-unsigned.apk upload -keypass $LOTTIE_UPLOAD_CERT_KEY_PASSWORD -signedjar LottieSample/build/outputs/apk/release/LottieSample-release-signed.apk
 
-if [ $? -ne 0 ]; then
-  exit 1
-end
+if [ $? -ne 0 ]; then exit $? end
 
 echo "----------Zipaligning APK"
 ${ANDROID_HOME}/build-tools/27.0.3/zipalign 4 LottieSample/build/outputs/apk/release/LottieSample-release-unsigned.apk LottieSample/build/outputs/apk/release/LottieSample-release-aligned.apk
 
-if [ $? -ne 0 ]; then
-  exit 1
-end
+if [ $? -ne 0 ]; then exit $? end
 
 ./gradlew :lottie:uploadArchives
-
-if [ $? -ne 0 ]; then
-  exit 1
-end
+exit $?
