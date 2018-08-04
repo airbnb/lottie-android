d41672042ec6b95205f44850fb68710314f171cf
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-04 13:22:42 -0700

    Fixed PR builds and added permission

--- sign.sh
+++ sign.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  # Don't sign PR builds.
+  exit 0
+fi
+
 echo "----------Signing APK"
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore lottie-upload-key.jks -storepass $LOTTIE_UPLOAD_CERT_KEY_STORE_PASSWORD LottieSample/build/outputs/apk/release/LottieSample-release-unsigned.apk upload -keypass $LOTTIE_UPLOAD_CERT_KEY_PASSWORD -signedjar LottieSample/build/outputs/apk/release/LottieSample-release-signed.apk
 
