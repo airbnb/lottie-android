a2cfda12731b968d0ec1537b9fffd37102c554c6
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-04 20:20:28 -0700

    Fixed path

--- gcloud_run.sh
+++ gcloud_run.sh
@@ -17,7 +17,7 @@ gcloud config set project lottie-snapshots
 
 RunTests()
 {
-gcloud firebase test android run --no-auto-google-login --type instrumentation --device model=Nexus5X,version=26 --app LottieSample/build/outputs/apk/release/LottieSample-release.apk --test LottieSample/build/outputs/apk/androidTest/debug/LottieSample-debug-androidTest.apk
+gcloud firebase test android run --no-auto-google-login --type instrumentation --device model=Nexus5X,version=26 --app LottieSample/build/outputs/apk/release/LottieSample-release-unsigned.apk --test LottieSample/build/outputs/apk/androidTest/debug/LottieSample-debug-androidTest.apk
 result=$?
 }
 
