895efc8deb9a7bc7582fd1902a58f9e8a7a6ff57
Gabriel Peal <gabriel@tonal.com>
2018-11-27 09:34:40 -0600

    Fixed oobe

--- lottie/src/main/java/com/airbnb/lottie/model/layer/SolidLayer.java
+++ lottie/src/main/java/com/airbnb/lottie/model/layer/SolidLayer.java
@@ -95,8 +95,8 @@ public class SolidLayer extends BaseLayer {
 
   @Override
   public List<Path> getPaths() {
-    if (paths.get(0) == null) {
-      paths.set(0, new Path());
+    if (paths.isEmpty()) {
+      paths.add(new Path());
     }
     paths.get(0).set(getPath());
     return paths;
