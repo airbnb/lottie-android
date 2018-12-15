9ac83764079f16ded683dc5f09f752a90da1a122
Gabriel Peal <gabriel@tonal.com>
2018-12-15 15:39:08 -0800

    Fix split paths

--- lottie/src/main/java/com/airbnb/lottie/model/animatable/AnimatableSplitDimensionPathValue.java
+++ lottie/src/main/java/com/airbnb/lottie/model/animatable/AnimatableSplitDimensionPathValue.java
@@ -6,22 +6,28 @@ import com.airbnb.lottie.animation.keyframe.BaseKeyframeAnimation;
 import com.airbnb.lottie.animation.keyframe.SplitDimensionPathKeyframeAnimation;
 import com.airbnb.lottie.value.Keyframe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AnimatableSplitDimensionPathValue implements AnimatableValue<PointF, PointF> {
   private final AnimatableFloatValue animatableXDimension;
   private final AnimatableFloatValue animatableYDimension;
 
+  private final ArrayList<Keyframe<Float>> keyframes;
+
   public AnimatableSplitDimensionPathValue(
       AnimatableFloatValue animatableXDimension,
       AnimatableFloatValue animatableYDimension) {
     this.animatableXDimension = animatableXDimension;
     this.animatableYDimension = animatableYDimension;
+    keyframes = new ArrayList<>(animatableXDimension.keyframes.size() + animatableYDimension.keyframes.size());
+    keyframes.addAll(animatableXDimension.keyframes);
+    keyframes.addAll(animatableYDimension.keyframes);
   }
 
   @Override
   public List<Keyframe<PointF>> getKeyframes() {
-    throw new UnsupportedOperationException("Cannot call getKeyframes on AnimatableSplitDimensionPathValue.");
+    throw keyframes;
   }
 
   @Override
