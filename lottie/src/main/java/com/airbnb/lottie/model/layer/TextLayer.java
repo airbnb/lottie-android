72f25bffb497612f28bbd481a3d7d96b92b56b57
Gabriel Peal <gpeal@users.noreply.github.com>
2018-12-16 13:33:18 -0800

    Allow text to be used as a mask (#1052)

--- lottie/src/main/java/com/airbnb/lottie/model/layer/TextLayer.java
+++ lottie/src/main/java/com/airbnb/lottie/model/layer/TextLayer.java
@@ -84,6 +84,12 @@ public class TextLayer extends BaseLayer {
     }
   }
 
+  @Override public void getBounds(RectF outBounds, Matrix parentMatrix, boolean applyParents) {
+    super.getBounds(outBounds, parentMatrix, applyParents);
+    // TODO: use the correct text bounds.
+    outBounds.set(0, 0, composition.getBounds().width(), composition.getBounds().height());
+  }
+
   @Override void drawLayer(Canvas canvas, Matrix parentMatrix, int parentAlpha) {
     canvas.save();
     if (!lottieDrawable.useTextGlyphs()) {
