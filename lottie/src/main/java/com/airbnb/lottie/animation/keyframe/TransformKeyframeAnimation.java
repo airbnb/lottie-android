c63587fca383a0e12ab6e879744a18610bf50de4
Gabriel Peal <gabriel@tonal.com>
2019-03-02 23:54:31 -0800

    Add default values for value callbacks

--- lottie/src/main/java/com/airbnb/lottie/animation/keyframe/TransformKeyframeAnimation.java
+++ lottie/src/main/java/com/airbnb/lottie/animation/keyframe/TransformKeyframeAnimation.java
@@ -280,43 +280,43 @@ public class TransformKeyframeAnimation {
     isIdentity = false;
     if (property == TRANSFORM_ANCHOR_POINT) {
       if (anchorPoint == null) {
-        anchorPoint = new ValueCallbackKeyframeAnimation(callback);
+        anchorPoint = new ValueCallbackKeyframeAnimation(callback, new PointF());
       } else {
         anchorPoint.setValueCallback((LottieValueCallback<PointF>) callback);
       }
     } else if (property == TRANSFORM_POSITION) {
       if (position == null) {
-        position = new ValueCallbackKeyframeAnimation(callback);
+        position = new ValueCallbackKeyframeAnimation(callback, new PointF());
       } else {
         position.setValueCallback((LottieValueCallback<PointF>) callback);
       }
     } else if (property == TRANSFORM_SCALE) {
       if (scale == null) {
-        scale = new ValueCallbackKeyframeAnimation(callback);
+        scale = new ValueCallbackKeyframeAnimation(callback, new ScaleXY());
       } else {
         scale.setValueCallback((LottieValueCallback<ScaleXY>) callback);
       }
     } else if (property == TRANSFORM_ROTATION) {
       if (rotation == null) {
-        rotation = new ValueCallbackKeyframeAnimation(callback);
+        rotation = new ValueCallbackKeyframeAnimation(callback, 0f);
       } else {
         rotation.setValueCallback((LottieValueCallback<Float>) callback);
       }
     } else if (property == TRANSFORM_OPACITY) {
       if (opacity == null) {
-        opacity = new ValueCallbackKeyframeAnimation(callback);
+        opacity = new ValueCallbackKeyframeAnimation(callback, 100);
       } else {
         opacity.setValueCallback((LottieValueCallback<Integer>) callback);
       }
     } else if (property == TRANSFORM_START_OPACITY && startOpacity != null) {
       if (startOpacity == null) {
-        startOpacity = new ValueCallbackKeyframeAnimation(callback);
+        startOpacity = new ValueCallbackKeyframeAnimation(callback, 100);
       } else {
         startOpacity.setValueCallback((LottieValueCallback<Float>) callback);
       }
     } else if (property == TRANSFORM_END_OPACITY && endOpacity != null) {
       if (endOpacity == null) {
-        endOpacity = new ValueCallbackKeyframeAnimation(callback);
+        endOpacity = new ValueCallbackKeyframeAnimation(callback, 100);
       } else {
         endOpacity.setValueCallback((LottieValueCallback<Float>) callback);
       }
--- lottie/src/main/java/com/airbnb/lottie/animation/keyframe/ValueCallbackKeyframeAnimation.java
+++ lottie/src/main/java/com/airbnb/lottie/animation/keyframe/ValueCallbackKeyframeAnimation.java
@@ -1,5 +1,6 @@
 package com.airbnb.lottie.animation.keyframe;
 
+import androidx.annotation.Nullable;
 import com.airbnb.lottie.value.Keyframe;
 import com.airbnb.lottie.value.LottieFrameInfo;
 import com.airbnb.lottie.value.LottieValueCallback;
@@ -9,9 +10,16 @@ import java.util.Collections;
 public class ValueCallbackKeyframeAnimation<K, A> extends BaseKeyframeAnimation<K, A> {
   private final LottieFrameInfo<A> frameInfo = new LottieFrameInfo<>();
 
+  private final A valueCallbackValue;
+
   public ValueCallbackKeyframeAnimation(LottieValueCallback<A> valueCallback) {
+    this(valueCallback, null);
+  }
+
+  public ValueCallbackKeyframeAnimation(LottieValueCallback<A> valueCallback, @Nullable A valueCallbackValue) {
     super(Collections.<Keyframe<K>>emptyList());
     setValueCallback(valueCallback);
+    this.valueCallbackValue = valueCallbackValue;
   }
 
   /**
@@ -30,8 +38,7 @@ public class ValueCallbackKeyframeAnimation<K, A> extends BaseKeyframeAnimation<
 
   @Override public A getValue() {
     //noinspection ConstantConditions
-    return valueCallback.getValueInternal(
-        0f, 0f, null, null, getProgress(), getProgress(), getProgress());
+    return valueCallback.getValueInternal(0f, 0f, valueCallbackValue, valueCallbackValue, getProgress(), getProgress(), getProgress());
   }
 
   @Override A getValue(Keyframe<K> keyframe, float keyframeProgress) {
