3ab5096e314d5c7f63331b48a95233680db708dd
Gabriel Peal <gabriel@tonal.com>
2019-06-02 19:28:27 +0200

    Added espresso tests

---
+++ espresso/src/main/java/com/airbnb/lottie/tests/espresso/LottieIdlingResource.kt
@@ -0,0 +1,30 @@
+package com.airbnb.lottie.tests.espresso
+
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
+import com.airbnb.lottie.LottieAnimationView
+
+class LottieIdlingResource(animationView: LottieAnimationView? = null, private val name: String = "Lottie") : IdlingResource {
+
+    init {
+        if (animationView != null) {
+            animationView.addLottieOnCompositionLoadedListener {
+                isIdle = true
+                callback?.onTransitionToIdle()
+                IdlingRegistry.getInstance().unregister(this)
+            }
+        }
+    }
+
+    private var callback: IdlingResource.ResourceCallback? = null
+    private var isIdle = false
+
+    override fun getName() = name
+
+    override fun isIdleNow() = isIdle
+
+    override fun registerIdleTransitionCallback(callback: IdlingResource.ResourceCallback) {
+        this.callback = callback
+        if (isIdle) callback.onTransitionToIdle()
+    }
+}
