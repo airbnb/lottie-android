7e64a9d68a7e58b6ea490fa9f8c8526612b02ad1
Gabriel Peal <gpeal@users.noreply.github.com>
2019-06-22 21:12:32 -0700

    Replace LottieAnimiationView JsonReader API with just InputStream and hide visibility of the Lottie Moshi JSON parsing classes. (#1279)
    This PR also hides the visibility of the Lottie Moshi JSON parsing classes.
    Fixes #1270

--- lottie/src/main/java/com/airbnb/lottie/LottieAnimationView.java
+++ lottie/src/main/java/com/airbnb/lottie/LottieAnimationView.java
@@ -32,6 +32,7 @@ import com.airbnb.lottie.value.LottieValueCallback;
 import com.airbnb.lottie.value.SimpleLottieValueCallback;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.util.HashSet;
 import java.util.List;
@@ -346,18 +347,18 @@ import static com.airbnb.lottie.RenderMode.SOFTWARE;
    * JSONObject never has to be done.
    */
   public void setAnimationFromJson(String jsonString, @Nullable String cacheKey) {
-    setAnimation(JsonReader.of(Okio.buffer(Okio.source(new ByteArrayInputStream(jsonString.getBytes())))), cacheKey);
+    setAnimation(new ByteArrayInputStream(jsonString.getBytes()), cacheKey);
   }
 
   /**
-   * Sets the animation from a JSONReader.
+   * Sets the animation from an arbitrary InputStream.
    * This will load and deserialize the file asynchronously.
    * <p>
    * This is particularly useful for animations loaded from the network. You can fetch the
    * bodymovin json from the network and pass it directly here.
    */
-  public void setAnimation(JsonReader reader, @Nullable String cacheKey) {
-    setCompositionTask(LottieCompositionFactory.fromJsonReader(reader, cacheKey));
+  public void setAnimation(InputStream stream, @Nullable String cacheKey) {
+    setCompositionTask(LottieCompositionFactory.fromJsonInputStream(stream, cacheKey));
   }
 
   /**
