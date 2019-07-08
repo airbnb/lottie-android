2e4395ccb4c470f8ccf0151703c7a79efe6ec815
gentrio <gentriolee@gmail.com>
2019-07-08 16:22:22 +0800

    delete unUseful test

--- LottieSample/src/androidTest/java/com/airbnb/lottie/samples/LottieTest.kt
+++ LottieSample/src/androidTest/java/com/airbnb/lottie/samples/LottieTest.kt
@@ -19,7 +19,6 @@ import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
 import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.view.updateLayoutParams
 import com.airbnb.lottie.FontAssetDelegate
 import com.airbnb.lottie.ImageAssetDelegate
 import com.airbnb.lottie.L
