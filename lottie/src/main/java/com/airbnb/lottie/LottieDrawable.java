da96f5c8d8d1ef530d6632e94a42933a739786a7
Gabriel Peal <gpeal@users.noreply.github.com>
2019-11-18 10:15:27 -0800

    Prevent a NPE when creating LottieAnimationView (#1434)

--- lottie/src/main/java/com/airbnb/lottie/LottieDrawable.java
+++ lottie/src/main/java/com/airbnb/lottie/LottieDrawable.java
@@ -753,6 +753,11 @@ public class LottieDrawable extends Drawable implements Drawable.Callback, Anima
   }
 
   public boolean isAnimating() {
+    // On some versions of Android, this is called from the LottieAnimationView constructor, before animator was created.
+    // https://github.com/airbnb/lottie-android/issues/1430
+    if (animator == null) {
+      return false;
+    }
     return animator.isRunning();
   }
 
