8b606cac676575d7f9d207b1fce9643685e00499
Bo Duan <boduan402@gmail.com>
2019-12-23 11:06:51 -0800

    Add unit test for XML initialized Lottie view. (#1437)
    To prevent issue like #1432 from happening again, add a unit test to verify the Lottie view initialization process via XML.
    
    One nit of this PR is the XML test file is included in aar. If there's better option to exclude it from release package, please advise.

---
+++ LottieSample/src/androidTest/java/com/airbnb/lottie/samples/LottieAnimationViewTest.java
@@ -0,0 +1,30 @@
+package com.airbnb.lottie.samples;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config
+@Ignore
+public class LottieAnimationViewTest {
+
+  private Activity activity;
+
+  @Before
+  public void setup(){
+    activity = Robolectric.buildActivity(Activity.class).create().get();
+  }
+
+  @Test
+  public void inflateShouldNotCrash() {
+    LayoutInflater.from(activity).inflate(R.layout.lottie_activity_main, null);
+  }
+}
