081da44146a1e892effc6be8899fed496a4973ad
Gabriel Peal <gpeal@users.noreply.github.com>
2020-02-15 18:38:16 -0800

    Fix time stretch and time remapping when there is a start offset (#1472)
    Fixes #1454
    Fixes #1456

--- lottie/src/main/java/com/airbnb/lottie/model/layer/BaseLayer.java
+++ lottie/src/main/java/com/airbnb/lottie/model/layer/BaseLayer.java
@@ -81,6 +81,8 @@ public abstract class BaseLayer
   @Nullable
   private MaskKeyframeAnimation mask;
   @Nullable
+  private FloatKeyframeAnimation inOutAnimation;
+  @Nullable
   private BaseLayer matteLayer;
   /**
    * This should only be used by {@link #buildParentLayerListIfNeeded()}
@@ -145,8 +147,7 @@ public abstract class BaseLayer
 
   private void setupInOutAnimations() {
     if (!layerModel.getInOutKeyframes().isEmpty()) {
-      final FloatKeyframeAnimation inOutAnimation =
-          new FloatKeyframeAnimation(layerModel.getInOutKeyframes());
+      inOutAnimation = new FloatKeyframeAnimation(layerModel.getInOutKeyframes());
       inOutAnimation.setIsDiscrete();
       inOutAnimation.addUpdateListener(new BaseKeyframeAnimation.AnimationListener() {
         @Override
@@ -523,6 +524,10 @@ public abstract class BaseLayer
     }
     if (layerModel.getTimeStretch() != 0) {
       progress /= layerModel.getTimeStretch();
+    }
+    if (inOutAnimation != null) {
+      // Time stretch needs to be divided again for the inOutAnimation.
+      inOutAnimation.setProgress(progress / layerModel.getTimeStretch());
     }
     if (matteLayer != null) {
       // The matte layer's time stretch is pre-calculated.
