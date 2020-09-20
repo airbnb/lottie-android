c77366d7dffa6a4b9041618fa31c297a87502d99
Denis Egorov <denis.egorov@corp.mail.ru>
2020-09-20 14:42:42 +0300

    delete context

--- lottie/src/main/java/com/airbnb/lottie/LottieConfig.java
+++ lottie/src/main/java/com/airbnb/lottie/LottieConfig.java
@@ -1,7 +1,5 @@
 package com.airbnb.lottie;
 
-import android.content.Context;
-
 import com.airbnb.lottie.network.LottieNetworkFetcher;
 import com.airbnb.lottie.network.LottieNetworkCacheProvider;
 
@@ -15,29 +13,20 @@ import androidx.annotation.Nullable;
  */
 public class LottieConfig {
 
-  @NonNull final Context applicationContext;
   @Nullable final LottieNetworkFetcher networkFetcher;
   @Nullable final LottieNetworkCacheProvider cacheProvider;
 
-  private LottieConfig(@NonNull Context applicationContext, @Nullable LottieNetworkFetcher networkFetcher, @Nullable
-      LottieNetworkCacheProvider cacheProvider) {
-    this.applicationContext = applicationContext;
+  private LottieConfig(@Nullable LottieNetworkFetcher networkFetcher, @Nullable LottieNetworkCacheProvider cacheProvider) {
     this.networkFetcher = networkFetcher;
     this.cacheProvider = cacheProvider;
   }
 
   public static final class Builder {
 
-    @NonNull
-    private final Context context;
     @Nullable
     private LottieNetworkFetcher networkFetcher;
     @Nullable
     private LottieNetworkCacheProvider cacheProvider;
-
-    public Builder(@NonNull Context context) {
-      this.context = context.getApplicationContext();
-    }
 
     @NonNull
     public Builder setNetworkFetcher(@NonNull LottieNetworkFetcher fetcher) {
@@ -63,7 +52,7 @@ public class LottieConfig {
 
     @NonNull
     public LottieConfig build() {
-      return new LottieConfig(context, networkFetcher, cacheProvider);
+      return new LottieConfig(networkFetcher, cacheProvider);
     }
   }
 }
