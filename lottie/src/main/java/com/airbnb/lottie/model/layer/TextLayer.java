f5517cd29b8ee5b3c60a7e27d97f4e8ee5088027
Marshall Weir <marshall.weir@gmail.com>
2020-10-01 22:24:02 -0400

    Fix scaling and tracking for dynamic text (#1635)
    Close #1183. Close #1310.
    
    This has a couple of related bugfixes.
    
    Fix the scale/positioning of the text relative to the parent (from PR #1583, thanks @pikaMouse)
    Include text tracking values in the line width used for text alignment
    Scale the text tracking values based on the font size rather than the parent size (parent size adjustment is already handled when we're calculating tracking values)
    Include all bytes for multibyte characters when calculating canvas offsets for characters.

--- lottie/src/main/java/com/airbnb/lottie/model/layer/TextLayer.java
+++ lottie/src/main/java/com/airbnb/lottie/model/layer/TextLayer.java
@@ -116,7 +116,7 @@ public class TextLayer extends BaseLayer {
   void drawLayer(Canvas canvas, Matrix parentMatrix, int parentAlpha) {
     canvas.save();
     if (!lottieDrawable.useTextGlyphs()) {
-      canvas.setMatrix(parentMatrix);
+      canvas.concat(parentMatrix);
     }
     DocumentData documentData = textAnimation.getValue();
     Font font = composition.getFonts().get(documentData.fontName);
@@ -260,13 +260,25 @@ public class TextLayer extends BaseLayer {
     // Line height
     float lineHeight = documentData.lineHeight * Utils.dpScale();
 
+    // Calculate tracking
+    float tracking = documentData.tracking / 10f;
+    if (trackingCallbackAnimation != null) {
+      tracking += trackingCallbackAnimation.getValue();
+    } else if (trackingAnimation != null) {
+      tracking += trackingAnimation.getValue();
+    }
+    tracking = tracking * Utils.dpScale() * textSize / 100.0f;
+
     // Split full text in multiple lines
     List<String> textLines = getTextLines(text);
     int textLineCount = textLines.size();
     for (int l = 0; l < textLineCount; l++) {
 
       String textLine = textLines.get(l);
-      float textLineWidth = strokePaint.measureText(textLine);
+      // We have to manually add the tracking between characters as the strokePaint ignores it
+      float textLineWidth = strokePaint.measureText(textLine) + (textLine.length() - 1) * tracking;
+
+      canvas.save();
 
       // Apply horizontal justification
       applyJustification(documentData.justification, canvas, textLineWidth);
@@ -277,10 +289,10 @@ public class TextLayer extends BaseLayer {
       canvas.translate(0, translateY);
 
       // Draw each line
-      drawFontTextLine(textLine, documentData, canvas, parentScale);
+      drawFontTextLine(textLine, documentData, canvas, tracking);
 
       // Reset canvas
-      canvas.setMatrix(parentMatrix);
+      canvas.restore();
     }
   }
 
@@ -292,20 +304,13 @@ public class TextLayer extends BaseLayer {
     return Arrays.asList(textLinesArray);
   }
 
-  private void drawFontTextLine(String text, DocumentData documentData, Canvas canvas, float parentScale) {
+  private void drawFontTextLine(String text, DocumentData documentData, Canvas canvas, float tracking) {
     for (int i = 0; i < text.length(); ) {
       String charString = codePointToString(text, i);
       i += charString.length();
       drawCharacterFromFont(charString, documentData, canvas);
-      float charWidth = fillPaint.measureText(charString, 0, 1);
-      // Add tracking
-      float tracking = documentData.tracking / 10f;
-      if (trackingCallbackAnimation != null) {
-        tracking += trackingCallbackAnimation.getValue();
-      } else if (trackingAnimation != null) {
-        tracking += trackingAnimation.getValue();
-      }
-      float tx = charWidth + tracking * parentScale;
+      float charWidth = fillPaint.measureText(charString);
+      float tx = charWidth + tracking;
       canvas.translate(tx, 0);
     }
   }
