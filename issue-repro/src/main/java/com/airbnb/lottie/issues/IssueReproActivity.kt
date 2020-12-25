3f855d9ed63ccf953d0a9c4bb200c6ef5b13af6b
Gabriel Peal <gpeal@users.noreply.github.com>
2020-12-25 14:51:37 -0800

    Minor cleanup to the issue-repro module (#1707)

--- issue-repro/src/main/java/com/airbnb/lottie/issues/IssueReproActivity.kt
+++ issue-repro/src/main/java/com/airbnb/lottie/issues/IssueReproActivity.kt
@@ -3,7 +3,7 @@ package com.airbnb.lottie.issues
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class IssueReproActivity : AppCompatActivity(R.layout.issue_repro_activity) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Reproduce any issues here.
