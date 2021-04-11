bcd74d30d1e5a65f472b5dd881a0a92ab17d52d2
Gabriel Peal <gabriel@tonal.com>
2021-04-11 14:17:32 -0700

    Add view binding to issue-repro

--- issue-repro/src/main/java/com/airbnb/lottie/issues/IssueReproActivity.kt
+++ issue-repro/src/main/java/com/airbnb/lottie/issues/IssueReproActivity.kt
@@ -2,10 +2,13 @@ package com.airbnb.lottie.issues
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.issues.databinding.IssueReproActivityBinding
 
-class IssueReproActivity : AppCompatActivity(R.layout.issue_repro_activity) {
+class IssueReproActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = IssueReproActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // Reproduce any issues here.
     }
 }
