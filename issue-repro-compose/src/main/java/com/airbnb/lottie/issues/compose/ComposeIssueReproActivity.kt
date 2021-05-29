ad4c68c069413954dbca8392115081b9c6dacc6f
Gabriel Peal <gabriel@tonal.com>
2021-05-29 15:44:44 -0700

    Reset issue repro activity

--- issue-repro-compose/src/main/java/com/airbnb/lottie/issues/compose/ComposeIssueReproActivity.kt
+++ issue-repro-compose/src/main/java/com/airbnb/lottie/issues/compose/ComposeIssueReproActivity.kt
@@ -3,13 +3,8 @@ package com.airbnb.lottie.issues.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import com.airbnb.lottie.compose.*
-import kotlinx.coroutines.isActive
+import androidx.compose.runtime.Composable
+import com.airbnb.lottie.compose.LottieAnimation
 
 class ComposeIssueReproActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,51 +16,6 @@ class ComposeIssueReproActivity : AppCompatActivity() {
 
     @Composable
     fun Content() {
-        var state by remember { mutableStateOf(0) }
-        val compositionResult = rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
-
-        val progress by lottieTransition(state) { progress ->
-            val composition = compositionResult.await()
-            when (state) {
-                0 -> animateLottieComposition(
-                    composition,
-                    progress,
-                    clipSpec = LottieAnimationClipSpec.MinAndMaxProgress(0f, 0.25f),
-                    cancellationBehavior = LottieCancellationBehavior.AtEnd,
-                )
-                1 -> {
-                    while (isActive) {
-                        animateLottieComposition(
-                            composition,
-                            progress,
-                            clipSpec = LottieAnimationClipSpec.MinAndMaxProgress(0.25f, 0.75f),
-                            cancellationBehavior = LottieCancellationBehavior.AtEnd,
-                        )
-                    }
-                }
-                else -> {
-                    animateLottieComposition(
-                        composition,
-                        progress,
-                        clipSpec = LottieAnimationClipSpec.MinAndMaxProgress(0.75f, 1f),
-                        cancellationBehavior = LottieCancellationBehavior.AtEnd,
-                    )
-                }
-            }
-        }
-
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            LottieAnimation(
-                compositionResult(),
-                progress,
-            )
-            TextButton(
-                onClick = { state = (state + 1) % 3 },
-            ) {
-                Text(state.toString())
-            }
-        }
+        LottieAnimation(null)
     }
 }
