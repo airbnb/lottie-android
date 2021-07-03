9f418806045ed86b5c77cc6cedab1df64b77e981
Gabriel Peal <gabriel@tonal.com>
2021-07-03 15:42:17 -0700

    Upgrade dependencies

--- sample/src/main/kotlin/com/airbnb/lottie/samples/model/AnimationDataV2.kt
+++ sample/src/main/kotlin/com/airbnb/lottie/samples/model/AnimationDataV2.kt
@@ -2,39 +2,16 @@ package com.airbnb.lottie.samples.model
 
 import android.os.Parcelable
 import com.airbnb.lottie.samples.utils.toColorIntSafe
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class AnimationDataV2(
-        val aepFlag: Int,
-        val baseprice: String,
-        val bgColor: String,
-        val bodymovinVersion: String?,
-        val createdAt: String,
-        val dataFile: String,
-        val description: String,
-        val downloads: Int,
-        val file: String,
-        val fileHash: String,
-        val id: Int,
-        val inProcess: Int,
-        val isSticker: Int,
-        val preview: String,
-        val previewFrame: Int,
-        val previewUrl: String,
-        val previewVideo: String,
-        val previewVideoUrl: String,
-        val price: Int,
-        val publishedAt: String,
-        val slug: String,
-        val speed: Int,
-        val status: Int,
-        val title: String,
-        val updated_at: String,
-        val userId: Int,
-        val videoFramerate: Int,
-        val views: Int
+        @SerializedName("bg_color") val bgColor: String,
+        @SerializedName("file") val file: String,
+        @SerializedName("id") val id: Int,
+        @SerializedName("preview") val preview: String,
+        @SerializedName("title") val title: String,
 ) : Parcelable {
     val bgColorInt get() = bgColor.toColorIntSafe()
-
 }
