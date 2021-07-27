de7515dce3387fef997cd3afcd1c93eed9a837f6
Gabriel Peal <gpeal@users.noreply.github.com>
2021-07-27 09:48:00 -0700

    [Compose] Make LottieCompositionSpec an inline class (#1855)
    Also update to rc01

--- lottie-compose/src/main/java/com/airbnb/lottie/compose/LottieCompositionSpec.kt
+++ lottie-compose/src/main/java/com/airbnb/lottie/compose/LottieCompositionSpec.kt
@@ -5,12 +5,12 @@ package com.airbnb.lottie.compose
  * A [com.airbnb.lottie.LottieComposition] is the stateless parsed version of a Lottie json file and is
  * passed into [rememberLottieComposition] or [LottieAnimation].
  */
-sealed class LottieCompositionSpec {
+sealed interface LottieCompositionSpec {
 
     /**
      * Load an animation from res/raw.
      */
-    data class RawRes(@androidx.annotation.RawRes val resId: Int) : LottieCompositionSpec()
+    inline class RawRes(@androidx.annotation.RawRes val resId: Int) : LottieCompositionSpec
 
     /**
      * Load an animation from the internet. Lottie has a default network stack that will use
@@ -23,22 +23,22 @@ sealed class LottieCompositionSpec {
      * passing this spec directly into [LottieAnimation] because it can fail and you want to
      * make sure that you properly handle the failures and/or retries.
      */
-    data class Url(val url: String) : LottieCompositionSpec()
+    inline class Url(val url: String) : LottieCompositionSpec
 
     /**
      * Load an animation from an arbitrary file. Make sure that your app has permissions to read it
      * or else this may fail.
      */
-    data class File(val fileName: String) : LottieCompositionSpec()
+    inline class File(val fileName: String) : LottieCompositionSpec
 
     /**
      * Load an animation from the assets directory of your app. This isn't type safe like [RawRes]
      * so make sure that the path to your animation is correct this will fail.
      */
-    data class Asset(val assetName: String) : LottieCompositionSpec()
+    inline class Asset(val assetName: String) : LottieCompositionSpec
 
     /**
      * Load an animation from its json string.
      */
-    data class JsonString(val jsonString: String) : LottieCompositionSpec()
+    inline class JsonString(val jsonString: String) : LottieCompositionSpec
 }
