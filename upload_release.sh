b3014306a62ebea65324a5a0f3930f5bf91580d2
Gabriel Peal <gabriel@tonal.com>
2022-05-30 17:26:39 -0700

    5.2.0

--- upload_release.sh
+++ upload_release.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./gradlew clean lottie:assembleRelease lottie-compose:assembleRelease lottie:publish lottie-compose:publish -DORG_GRADLE_PROJECT_mavenCentralUsername="${SONATYPE_USERNAME}" -DORG_GRADLE_PROJECT_mavenCentralPassword="${SONATYPE_PASSWORD}" --rerun-tasks --no-parallel
+git_branch=`git rev-parse --abbrev-ref HEAD`
+if [ "$git_branch" != "master" ]; then
+    echo "You must run this from master!"
+    exit 1
+fi
+./gradlew clean lottie:assembleRelease lottie-compose:assembleRelease lottie:publish lottie-compose:publish -DORG_GRADLE_PROJECT_mavenCentralUsername="${SONATYPE_USERNAME}" -DORG_GRADLE_PROJECT_mavenCentralPassword="${SONATYPE_PASSWORD}" --rerun-tasks --no-parallel --no-configuration-cache
