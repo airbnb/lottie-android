63a16357b8a36464ac346776a644b4085d3c75e9
Gabriel Peal <gabriel@watershedclimate.com>
2022-11-11 18:56:42 -0800

    Fixed multiline

--- lottie/src/main/java/com/airbnb/lottie/model/layer/TextLayer.java
+++ lottie/src/main/java/com/airbnb/lottie/model/layer/TextLayer.java
@@ -194,9 +194,7 @@ public class TextLayer extends BaseLayer {
       // Apply horizontal justification
       applyJustification(documentData, canvas, textLineWidth);
 
-      // Center text vertically
-      float multilineTranslateY = (textLineCount - 1) * lineHeight / 2;
-      float translateY = l * lineHeight - multilineTranslateY;
+      float translateY = l * (documentData.lineHeight - documentData.size) * Utils.dpScale();
       canvas.translate(0, translateY);
       if (documentData.boxPosition != null) {
         canvas.translate(documentData.boxPosition.x, documentData.boxPosition.y);
