c6bf15d8ef9ce638bbc45687d33702fcabf7c650
Gabriel Peal <gabriel@gpeal.com>
2023-12-29 17:21:47 -0800

    Cleanup

--- lottie-compose/src/main/java/com/airbnb/lottie/compose/LottiePainter.kt
+++ lottie-compose/src/main/java/com/airbnb/lottie/compose/LottiePainter.kt
@@ -3,9 +3,11 @@ package com.airbnb.lottie.compose
 import android.graphics.Matrix
 import android.graphics.Typeface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
@@ -17,9 +19,11 @@ import com.airbnb.lottie.AsyncUpdates
 import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.LottieDrawable
 import com.airbnb.lottie.RenderMode
-import com.airbnb.lottie.utils.Utils
 import kotlin.math.roundToInt
 
+/**
+ * A composable that makes it easy to create a [LottiePainter] and update its properties.
+ */
 @Composable
 fun rememberLottiePainter(
     composition: LottieComposition? = null,
@@ -36,22 +40,25 @@ fun rememberLottiePainter(
     asyncUpdates: AsyncUpdates = AsyncUpdates.AUTOMATIC,
 ): LottiePainter {
     val painter = remember { LottiePainter() }
-    painter.composition.value = composition
-    painter.progress.floatValue = progress
-    painter.outlineMasksAndMattes.value = outlineMasksAndMattes
-    painter.applyOpacityToLayers.value = applyOpacityToLayers
-    painter.enableMergePaths.value = enableMergePaths
-    painter.renderMode.value = renderMode
-    painter.maintainOriginalImageBounds.value = maintainOriginalImageBounds
-    painter.dynamicProperties.value = dynamicProperties
-    painter.clipToCompositionBounds.value = clipToCompositionBounds
-    painter.clipTextToBoundingBox.value = clipTextToBoundingBox
-    painter.fontMap.value = fontMap
-    painter.asyncUpdates.value = asyncUpdates
+    painter.composition = composition
+    painter.progress = progress
+    painter.outlineMasksAndMattes = outlineMasksAndMattes
+    painter.applyOpacityToLayers = applyOpacityToLayers
+    painter.enableMergePaths = enableMergePaths
+    painter.renderMode = renderMode
+    painter.maintainOriginalImageBounds = maintainOriginalImageBounds
+    painter.dynamicProperties = dynamicProperties
+    painter.clipToCompositionBounds = clipToCompositionBounds
+    painter.clipTextToBoundingBox = clipTextToBoundingBox
+    painter.fontMap = fontMap
+    painter.asyncUpdates = asyncUpdates
     return painter
 }
 
-class LottiePainter(
+/**
+ * A [Painter] that renders a [LottieComposition].
+ */
+class LottiePainter internal constructor(
     composition: LottieComposition? = null,
     progress: Float = 0f,
     outlineMasksAndMattes: Boolean = false,
@@ -65,30 +72,31 @@ class LottiePainter(
     fontMap: Map<String, Typeface>? = null,
     asyncUpdates: AsyncUpdates = AsyncUpdates.AUTOMATIC,
 ) : Painter() {
-    val composition = mutableStateOf(composition)
-    val progress = mutableFloatStateOf(progress)
-    val outlineMasksAndMattes = mutableStateOf(outlineMasksAndMattes)
-    val applyOpacityToLayers = mutableStateOf(applyOpacityToLayers)
-    val enableMergePaths = mutableStateOf(enableMergePaths)
-    val renderMode = mutableStateOf(renderMode)
-    val maintainOriginalImageBounds = mutableStateOf(maintainOriginalImageBounds)
-    val dynamicProperties = mutableStateOf(dynamicProperties)
+    internal var composition by mutableStateOf(composition)
+    internal var progress by mutableFloatStateOf(progress)
+    internal var outlineMasksAndMattes by mutableStateOf(outlineMasksAndMattes)
+    internal var applyOpacityToLayers by mutableStateOf(applyOpacityToLayers)
+    internal var enableMergePaths by mutableStateOf(enableMergePaths)
+    internal var renderMode by mutableStateOf(renderMode)
+    internal var maintainOriginalImageBounds by mutableStateOf(maintainOriginalImageBounds)
+    internal var dynamicProperties by mutableStateOf(dynamicProperties)
+    internal var clipToCompositionBounds by mutableStateOf(clipToCompositionBounds)
+    internal var fontMap by mutableStateOf(fontMap)
+    internal var asyncUpdates by mutableStateOf(asyncUpdates)
+    internal var clipTextToBoundingBox by mutableStateOf(clipTextToBoundingBox)
+
     private var setDynamicProperties: LottieDynamicProperties? = null
-    val clipToCompositionBounds = mutableStateOf(clipToCompositionBounds)
-    val fontMap = mutableStateOf(fontMap)
-    val asyncUpdates = mutableStateOf(asyncUpdates)
-    val clipTextToBoundingBox = mutableStateOf(clipTextToBoundingBox)
 
     private val drawable = LottieDrawable()
     private val matrix = Matrix()
     override val intrinsicSize: Size
         get() {
-            val composition = composition.value ?: return Size.Unspecified
+            val composition = composition ?: return Size.Unspecified
             return Size(composition.bounds.width().toFloat(), composition.bounds.height().toFloat())
         }
 
     override fun DrawScope.onDraw() {
-        val composition = composition.value ?: return
+        val composition = composition ?: return
         drawIntoCanvas { canvas ->
             val compositionSize = Size(composition.bounds.width().toFloat(), composition.bounds.height().toFloat())
             val intSize = IntSize(size.width.roundToInt(), size.height.roundToInt())
@@ -96,22 +104,22 @@ class LottiePainter(
             matrix.reset()
             matrix.preScale(intSize.width / compositionSize.width, intSize.height / compositionSize.height)
 
-            drawable.enableMergePathsForKitKatAndAbove(enableMergePaths.value)
-            drawable.renderMode = renderMode.value
-            drawable.asyncUpdates = asyncUpdates.value
+            drawable.enableMergePathsForKitKatAndAbove(enableMergePaths)
+            drawable.renderMode = renderMode
+            drawable.asyncUpdates = asyncUpdates
             drawable.composition = composition
-            drawable.setFontMap(fontMap.value)
-            if (dynamicProperties.value !== setDynamicProperties) {
+            drawable.setFontMap(fontMap)
+            if (dynamicProperties !== setDynamicProperties) {
                 setDynamicProperties?.removeFrom(drawable)
-                dynamicProperties.value?.addTo(drawable)
-                setDynamicProperties = dynamicProperties.value
+                dynamicProperties?.addTo(drawable)
+                setDynamicProperties = dynamicProperties
             }
-            drawable.setOutlineMasksAndMattes(outlineMasksAndMattes.value)
-            drawable.isApplyingOpacityToLayersEnabled = applyOpacityToLayers.value
-            drawable.maintainOriginalImageBounds = maintainOriginalImageBounds.value
-            drawable.clipToCompositionBounds = clipToCompositionBounds.value
-            drawable.clipTextToBoundingBox = clipTextToBoundingBox.value
-            drawable.progress = progress.floatValue
+            drawable.setOutlineMasksAndMattes(outlineMasksAndMattes)
+            drawable.isApplyingOpacityToLayersEnabled = applyOpacityToLayers
+            drawable.maintainOriginalImageBounds = maintainOriginalImageBounds
+            drawable.clipToCompositionBounds = clipToCompositionBounds
+            drawable.clipTextToBoundingBox = clipTextToBoundingBox
+            drawable.progress = progress
             drawable.setBounds(0, 0, composition.bounds.width(), composition.bounds.height())
             drawable.draw(canvas.nativeCanvas, matrix)
         }
