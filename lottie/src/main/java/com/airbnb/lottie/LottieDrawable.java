airbnb/lottie-android

commit 9527586bd77e648704679537ef1bf90869a31381

Apply DP scaling to canvas in software rendering
When rendering a view, the Android graphics stack, by convention, gives
it a canvas where the transformation to put the view in the right
position is applied. It also allows the view to draw with its intrinsic
width and height in pixels, and the canvas has an inverse scaling
transform to draw to the final surface with the right size.

Thus, the transform matrices we work with during hardware rendering
exclude this inverse scaling.

In software rendering mode, we bake this scaling into the root matrix,
which causes different values to appear in software vs hardware
rendering, which:
* Has an impact on the final image due to e.g. integer rounding
* Makes debugging HW/SW differences trickier, as values are different.

To make the modes closer, "unbake" the pre-existing scale of the canvas
from the initial root matrix.

To mitigate risk, this is only done for scale, and not for the full
matrix itself, as there is code which might interact with other types of
transforms in unexpected ways. (Whereas scaling is a "safer" one.) This
can be done in the future to achieve 100% equal values between the two
modes.

lottie/src/main/java/com/airbnb/lottie/LottieDrawable.java
@@ -173,6 +173,7 @@ public class LottieDrawable extends Drawable implements Drawable.Callback, Anima
   private RectF softwareRenderingDstBoundsRectF;
   private RectF softwareRenderingTransformedBounds;
   private Matrix softwareRenderingOriginalCanvasMatrix;
+  private float[] softwareRenderingOriginalCanvasMatrixElements = new float[9];
   private Matrix softwareRenderingOriginalCanvasMatrixInverse;
 
   /**
@@ -1803,13 +1804,20 @@ public class LottieDrawable extends Drawable implements Drawable.Callback, Anima
     ensureSoftwareRenderingBitmap(renderWidth, renderHeight);
 
     if (isDirty) {
+      softwareRenderingOriginalCanvasMatrix.getValues(softwareRenderingOriginalCanvasMatrixElements);
+      float preExistingScaleX = softwareRenderingOriginalCanvasMatrixElements[Matrix.MSCALE_X];
+      float preExistingScaleY = softwareRenderingOriginalCanvasMatrixElements[Matrix.MSCALE_Y];
+
       renderingMatrix.set(softwareRenderingOriginalCanvasMatrix);
-      renderingMatrix.preScale(scaleX, scaleY);
+      renderingMatrix.preScale(scaleX / preExistingScaleX, scaleY / preExistingScaleY);
+
       // We want to render the smallest bitmap possible. If the animation doesn't start at the top left, we translate the canvas and shrink the
       // bitmap to avoid allocating and copying the empty space on the left and top. renderWidth and renderHeight take this into account.
       renderingMatrix.postTranslate(-softwareRenderingTransformedBounds.left, -softwareRenderingTransformedBounds.top);
 
       softwareRenderingBitmap.eraseColor(0);
+      softwareRenderingCanvas.setMatrix(Matrix.IDENTITY_MATRIX);
+      softwareRenderingCanvas.scale(preExistingScaleX, preExistingScaleY);
       compositionLayer.draw(softwareRenderingCanvas, renderingMatrix, alpha, null);
 
       // Calculate the dst bounds.

commit 9b51a7b7be647595f32eaac48be9847f6ebc2cfc

Fix transformation in software rendering
Commit 9527586b introduced an issue with transformation order, causing
View-wide translations to be misapplied in software rendering.

lottie/src/main/java/com/airbnb/lottie/LottieDrawable.java
@@ -1809,14 +1809,15 @@ public class LottieDrawable extends Drawable implements Drawable.Callback, Anima
       float preExistingScaleY = softwareRenderingOriginalCanvasMatrixElements[Matrix.MSCALE_Y];
 
       renderingMatrix.set(softwareRenderingOriginalCanvasMatrix);
-      renderingMatrix.preScale(scaleX / preExistingScaleX, scaleY / preExistingScaleY);
+      renderingMatrix.preScale(scaleX, scaleY);
+      renderingMatrix.postScale(1.0f / preExistingScaleX, 1.0f / preExistingScaleY);
 
       // We want to render the smallest bitmap possible. If the animation doesn't start at the top left, we translate the canvas and shrink the
       // bitmap to avoid allocating and copying the empty space on the left and top. renderWidth and renderHeight take this into account.
       renderingMatrix.postTranslate(-softwareRenderingTransformedBounds.left, -softwareRenderingTransformedBounds.top);
 
       softwareRenderingBitmap.eraseColor(0);
-      softwareRenderingCanvas.setMatrix(Matrix.IDENTITY_MATRIX);
+      softwareRenderingCanvas.setMatrix(new Matrix());
       softwareRenderingCanvas.scale(preExistingScaleX, preExistingScaleY);
       compositionLayer.draw(softwareRenderingCanvas, renderingMatrix, alpha, null);
 